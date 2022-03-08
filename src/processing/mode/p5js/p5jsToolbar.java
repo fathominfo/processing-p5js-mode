f1090eaca79a2d2294f581ae31b841837dfd790a
Ben Fry <ben@fathom.info>
2022-03-08 11:58:57 -0500

    almost none of this code was being used; cleaned it up

--- src/processing/mode/p5js/p5jsToolbar.java
+++ src/processing/mode/p5js/p5jsToolbar.java
@@ -1,29 +1,14 @@
 package processing.mode.p5js;
 
-import java.awt.event.MouseEvent;
-
-import javax.swing.JPopupMenu;
-
 import processing.app.ui.Editor;
 import processing.app.ui.EditorToolbar;
 
 
 public class p5jsToolbar extends EditorToolbar {
-  static protected final int RUN    = 0;
-  static protected final int STOP   = 1;
-
-  static protected final int NEW    = 2;
-  static protected final int OPEN   = 3;
-  static protected final int SAVE   = 4;
-  static protected final int EXPORT = 5;
-
 
   public p5jsToolbar(Editor editor) {
     super(editor);
   }
-
-
-  public void init() { }
 
 
   @Override
@@ -37,51 +22,5 @@ public class p5jsToolbar extends EditorToolbar {
   public void handleStop() {
     p5jsEditor jsEditor = (p5jsEditor) editor;
     jsEditor.handleStop();
-  }
-
-
-  public void handlePressed(MouseEvent e, int index) {
-    switch (index) {
-
-    case RUN:
-      handleRun(e.getModifiers());
-      break;
-
-    case STOP:
-      handleStop();
-      break;
-
-    case OPEN:
-      JPopupMenu popup = editor.getMode().getToolbarMenu().getPopupMenu();
-      popup.show(this, e.getX(), e.getY());
-      break;
-
-    case NEW:
-      base.handleNew();
-      break;
-
-    case SAVE:
-      editor.handleSave(false);
-      break;
-
-      /*
-    case EXPORT:
-      jsEditor.handleExport( true );
-      break;
-      */
-    }
-  }
-
-
-  static public String getTitle (int index, boolean shift) {
-    switch (index) {
-    case RUN:    return "Start server";
-    case STOP:   return "Stop server";
-    case NEW:    return "New";
-    case OPEN:   return "Open";
-    case SAVE:   return "Save";
-    case EXPORT: return "Export for Web";
-    }
-    return null;
   }
 }
